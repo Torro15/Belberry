d6537ee01fab1f119fad993bbf0a17365c4bdc58
Torro15 <svetlgrish28@gmail.com>
2024-09-20 16:26:27 +0300

    swiper-3

--- js/main.js
+++ js/main.js
@@ -122,6 +122,32 @@ var swiper2 = new Swiper(".mySwiper-2", {
     }
 });
 
+
+var swiper3 = new Swiper(".mySwiper-3", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true, 
+
+    breakpoints: {
+        320: { // Для экранов шириной шириной 320 и больше
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        640: { // Для экранов шириной 640px и больше
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: { // Для экранов шириной 1024px и больше
+            slidesPerView: 4,
+            spaceBetween: 20,
+        }
+    }
+});
+
 var swiper4 = new Swiper(".mySwiper-4", {
     slidesPerView: 3,
     spaceBetween: 20,
